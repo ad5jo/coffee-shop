54f4ec14768ed439e1aa03de978a04b5c97e4857
Dave Durkee <ad5jo@yahoo.com>
2019-03-09 23:28:25 -0600

    finished 9. Classe based components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 // , { Component }
 //import logo from './logo.svg';
 import './App.css';
+class App extends React.Component {
+  render(){
+    const sidebarTitle = 'Daily Menu'
+    const rightnow = new Date().toString().split(" ").slice(0,4).join(" ")
 
-function App() {
+    const menu = {
+      coffee: [
+        {name: 'expresso', price: 3.99},
+        {name: 'Cafe Latte', price: 4.99},
+        {name: 'Drip Coffee', price: 2.99},
+        {name: 'Iced Coffee', price: 3.99},
+        {name: 'Cold Brew', price: 2.99},
+        {name: 'Americano', price: 1.99},
+        {name: 'Cappucino', price: 3.99},
+        {name: 'Mocha', price: 3.50},
+      ],
+      bakery: [
+        {name: 'Blue berry muffin', price: 2.00},
+        {name: 'Banana Breat', price: 2.00},
+        {name: 'Lemon Pound Cake', price: 2.00},
+      ],
+      specials: [
+        {name: 'pie', price: 2.00},
+        {name: 'Charro Beans', price: 4.00},
+        {name: 'Tacos', price: 6.00},
+      ],
 
-  const sidebarTitle = 'Daily Menu'
-  const rightnow = new Date().toString().split(" ").slice(0,4).join(" ")
-
-  const menu = {
-    coffee: [
-      {name: 'expresso', price: 3.99},
-      {name: 'Cafe Latte', price: 4.99},
-      {name: 'Drip Coffee', price: 2.99},
-      {name: 'Iced Coffee', price: 3.99},
-      {name: 'Cold Brew', price: 2.99},
-      {name: 'Americano', price: 1.99},
-      {name: 'Cappucino', price: 3.99},
-      {name: 'Mocha', price: 3.50},
-    ],
-    bakery: [
-      {name: 'Blue berry muffin', price: 2.00},
-      {name: 'Banana Breat', price: 2.00},
-      {name: 'Lemon Pound Cake', price: 2.00},
-    ],
-    specials: [
-      {name: 'pie', price: 2.00},
-      {name: 'Charro Beans', price: 4.00},
-      {name: 'Tacos', price: 6.00},
-    ],
-
+    }
+    return(
+      <div className="App">
+        <Navbar />
+        <Sidebar 
+          title={sidebarTitle} 
+          now={rightnow} 
+          menu = {menu}
+        />
+        <Main />
+        <Footer />
+      </div>
+    )
   }
-
-
-  return(
-    <div className="App">
-      <Navbar />
-      <Sidebar 
-        title={sidebarTitle} 
-        now={rightnow} 
-        menu = {menu}
-      />
-      <Main />
-      <Footer />
-    </div>
-  )
 }
+
+
+
 
 function Navbar(props){
   return (
